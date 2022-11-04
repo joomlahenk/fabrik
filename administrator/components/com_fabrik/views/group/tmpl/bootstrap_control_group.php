2ed47d921df3103bffaeb14fd84089cfba3ea10b
troester <trob@users.noreply.github.com>
2022-11-04 09:58:09 +0100

    group admin form control-group

--- administrator/components/com_fabrik/views/group/tmpl/bootstrap_control_group.php
+++ administrator/components/com_fabrik/views/group/tmpl/bootstrap_control_group.php
@@ -20,7 +20,7 @@ defined('_JEXEC') or die('Restricted access');
 		<?php echo $this->field->label; ?>
 	</div>
 <?php endif; ?>
-	<div>
+	<div style="width:75%">
 		<?php echo $this->field->input; ?>
 	</div>
 </div>
