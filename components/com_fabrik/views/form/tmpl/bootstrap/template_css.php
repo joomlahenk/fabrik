ce980ad0c4770492ecfa3cb88489177663aabe4f
troester <trob@users.noreply.github.com>
2022-11-11 18:49:20 +0100

    Update template_css.php

--- components/com_fabrik/views/form/tmpl/bootstrap/template_css.php
+++ components/com_fabrik/views/form/tmpl/bootstrap/template_css.php
@@ -20,10 +20,6 @@ clear: left;
     color: var(--body-color);
     font-size: 1rem;
 }
-/*BS5 ajax validation: icons overriding dropdown caret*/
-.fabrikinput.form-select {
-    background-position: right 1rem center, center right 0.1rem !important;
-	 padding-right:0 !important;
-}
+
 ";
 ?>
